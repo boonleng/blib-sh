89c30b76459334d9aeb951577a4eb5fcd3d43d57
boonleng <boonleng@ou.edu>
2015-07-03 10:13:28 -0500

    Added tconfig.sh in the collection

---
+++ tconfig.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Start port number for SSH connections
+p=20000
+
+# Start port number for VNC connections
+q=55900
+
+# Initialize an empty string
+str=""
+
+# Make N pull down from the SSH server
+for ((i=0; i<4; i++)); do
+	str="$str -L $p:localhost:$p";
+	str="$str -L $q:localhost:$q";
+	p=$((p+1))
+	q=$((q+1))
+done
+
+for ((i=22000; i<22003; i++)); do
+	str="$str -L $i:localhost:$i";
+done
+
+while read line; do
+	if [ $line ]; then
+		str="$str -L $p:$line:22";
+		p=$((p+1))
+	 fi
+done < $HOME/bin/hosts.sshfs
+
+echo $str
+
