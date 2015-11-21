a3a9b240b71d7e3100bb95dc300d804e45a2e8cc
boonleng <boonleng@ou.edu>
2015-11-21 16:31:41 -0600

    Links are generated based on file list from NCDC

--- ncdc.sh
+++ ncdc.sh
@@ -11,7 +11,29 @@ echo -e "Creating directory \033[1;33m~/Downloads/${order}\033[0m for download .
 mkdir -p ~/Downloads/${order}
 cd ~/Downloads/${order}
 
-wget -r -q -l 2 -nd -np --show-progress --reject index* -A *.gz ${1}
+# Get the filelist
+wget -q -nd -np ${1%/}/fileList.txt
+
+# Go through the filelist to get all the files
+if [ ! -f fileList.txt ]; then
+	# Do the old school method, get everything in the folder
+	wget -r -q -l 2 -nd -np --show-progress --reject index* -A *.gz ${1}
+else
+	i=0
+	n=$(wc -l fileList.txt)
+	n=${n% *}
+	n=$((n+0))
+	while read file; do
+		url="${1%/}/${file}"
+		if [[ -z ${i} || -z ${n} ]]; then
+			n=$(wc -l fileList.txt)
+			i=0
+		fi
+		echo "${i} of ${n} ${url}"
+		wget -q --show-progress -nd -np ${1%/}/${file}
+		i=$((i+1))
+	done < fileList.txt
+fi
 
 cd - >/dev/null
 
