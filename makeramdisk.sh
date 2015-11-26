3ed5257d64b04d4b0d20b45279195eee550d1536
boonleng <boonleng@ou.edu>
2015-11-26 12:40:30 -0600

    Upgraded makeramdisk.sh to be more customizable.

--- makeramdisk.sh
+++ makeramdisk.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-Volume="RAMDisk"
-SizeInMB=512
-NumSectors=$((2*1024*SizeInMB))
-DeviceName=`hdid -nomount ram://$NumSectors`
-echo $DeviceName
-diskutil eraseVolume HFS+ RAMDisk $DeviceName
+
+# The capacity in MB
+if [ -z "$1" ]; then
+	sizeInMB=512
+else
+	sizeInMB=$1
+fi
+
+# Custom volume name
+if [ -z "$2" ]; then
+	volume="RAMDisk"
+else
+	volume="$2"
+fi
+
+numSectors=$((2 * 1024 * sizeInMB))
+deviceName=`hdid -nomount ram://${numSectors}`
+deviceName=${deviceName%% *}
+
+echo -e "Creating \033[1;35m${volume}\033[0m at \033[1;33m${deviceName}\033[0m ..."
+
+diskutil eraseVolume HFS+ ${volume} ${deviceName}
