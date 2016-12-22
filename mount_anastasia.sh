142bb8725bc7546a0b0051d8e6017cf701240371
boonleng <boonleng@ou.edu>
2016-12-22 07:20:35 -0600

    Added the reverse tunnel script

--- mount_anastasia.sh
+++ mount_anastasia.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# This function respects the SSH login profile in .ssh/config
 
+# If no hostname is supplied, the default is anastasia (main host of PX-10k)
 if [ -z ${1} ]; then
 	DEST=anastasia
 else
@@ -13,5 +15,5 @@ fi
 
 options="-o auto_cache,reconnect,follow_symlinks,defer_permissions,negative_vncache,volname=${DEST}"
 
-sshfs root@${DEST}:/ /Volumes/${DEST} ${options}
+sshfs ${DEST}:/ /Volumes/${DEST} ${options}
 
---
+++ rtun.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+. $HOME/bin/boonlib.sh
+
+LOGFILE=$HOME/log/rtun.log
+
+# Login and External system
+USERHOST="cheo4524@starbuck.nwc.ou.edu"
+
+if [[ $HOSTNAME == "cerulean.local" ]]; then
+	COMMAND="ssh -f -N -R 20000:localhost:22 -R 55900:localhost:5900 -R 20001:morning.local:22 -R 55901:morning.local:5900 -R 20002:tiffany.local:22 -R 55902:tiffany.local:5900 -R 20003:10.203.7.76:22 $USERHOST >/dev/null 2>&1"
+	TEST="ssh $USERHOST netstat -an | egrep "tcp.*:20000.*LISTEN" >/dev/null 2>&1"
+elif [[ $HOSTNAME == "celeste.local" ]]; then
+	COMMAND="ssh -f -N -R 22000:localhost:22 -R 22001:cerulean.local:22 $USERHOST >/dev/null 2>&1"
+	TEST="ssh $USERHOST netstat -an | egrep "tcp.*:22000.*LISTEN" >/dev/null 2>&1"
+fi
+
+ps ax | grep -e "${COMMAND:0:20}" | grep -v grep > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+	msg="No tunnel found. Respawning ..."
+	$COMMAND &
+	$TEST
+	if [ $? -ne 0 ]; then
+		msg="${msg} failed. Try again later."
+	else
+		msg="${msg} ok."
+	fi
+else
+	$TEST
+	if [ $? -ne 0 ] ; then
+		msg="Stalled tunnel. Respawning ..."
+		pid=$(ps ax | grep -e "${COMMAND:0:20}" | grep -v grep | awk '{ print $1 }')
+		sudo kill -9 $pid >/dev/null 2>&1
+		$COMMAND &
+		$TEST
+		if [ $? -ne 0 ]; then
+			msg="${msg} failed. Try again later."
+		else
+			msg="${msg} ok."
+		fi
+	else
+		msg="Tunnel okay."
+	fi
+fi
+
+log ${msg}
+
