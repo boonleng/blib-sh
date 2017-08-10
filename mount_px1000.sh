76f1c3c91b0274321eb15eb8f8e0c0117f73033c
boonleng <boonleng@ou.edu>
2017-08-10 10:59:47 -0500

    Added IP address in the comment section

--- mount_px1000.sh
+++ mount_px1000.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 # To make this script works properly in he RIL, add the following
-# to your hosts file
+# to ~/.ssh/config
 #
-# 10.203.6.248    azure
-# 10.203.6.248    burne
-# 10.203.6.248    woodstock
+# Host azure
+#     Hostname 10.203.6.248
+#     User boonleng
+#     Port 2202
+# Host woodstock
+#     Hostname 10.203.6.248
+#     User boonleng
+#     Port 2203
+# Host burne
+#     Hostname 10.203.6.248
+#     User root
+#     Port 2204
 
 . ${HOME}/boonlib-sh/boonlib.sh
 
 mount_host azure
 mount_host burne
 mount_host woodstock
-
