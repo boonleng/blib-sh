7dcb67340aec0815343a7899a473ba96b3330cf1
boonleng <boonleng@ou.edu>
2017-12-03 11:29:28 -0600

    Removed the machine is is no longer valid

--- rtun.sh
+++ rtun.sh
@@ -10,9 +10,6 @@ USERHOST="cheo4524@starbuck.nwc.ou.edu"
 if [[ $HOSTNAME == "cerulean.local" ]]; then
 	COMMAND="ssh -f -N -R 20000:localhost:22 -R 55900:localhost:5900 -R 20001:dawn.local:22 -R 55901:dawn.local:5900 -R 20002:tiffany.local:22 -R 55902:tiffany.local:5900 -R 20003:dodger.local:22 -R 55903:dodger.local:5900 $USERHOST >/dev/null 2>&1"
 	TEST="ssh $USERHOST netstat -an | egrep "tcp.*:20000.*LISTEN" >/dev/null 2>&1"
-elif [[ $HOSTNAME == "celeste.local" ]]; then
-	COMMAND="ssh -f -N -R 22000:localhost:22 -R 22001:cerulean.local:22 $USERHOST >/dev/null 2>&1"
-	TEST="ssh $USERHOST netstat -an | egrep "tcp.*:22000.*LISTEN" >/dev/null 2>&1"
 fi
 
 ps ax | grep -e "${COMMAND:0:20}" | grep -v grep > /dev/null 2>&1
