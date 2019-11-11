c38b126b76e21ad8d1d842c3f5fd73dfad572440
boonleng <boonleng@ou.edu>
2019-11-11 16:54:20 -0600

    Changed to key substitution

--- victoria-clone.sh
+++ victoria-clone.sh
@@ -8,47 +8,70 @@ dst_base="sda"
 label="victoria"
 
 # Hyper parameters
-size1=$((60 * 1024 * 1024 / 512))
+debug=false
+size1=$((64 * 1024 * 1024 / 512))
 total_size=$((1280 * 1024 * 1024 / 512))
 
 sfd0=$(sfdisk -d /dev/${src_dev})
 
-# echo "${sfd0}"
+echo "size1 = ${size1}   total_size = ${total_size}"
+echo ""
+echo -e "\033[38;5;211mSource partition scheme\033[m"
+echo "${sfd0}"
 
-sfd1=$(echo "${sfd0}" | sed -e "\/dev\/${src_base}1/s/size=[^,]*/size= ${size1}/")
+sfd1=$(echo "${sfd0}" | sed -e "\/dev\/${src_base}2/s/start=[^,]*/start= P2_START/" -e "\/dev\/${src_base}2/s/size=[^,]*/start= P2_SIZE/")
 
-# echo "${sfd1}"
+if [ "${debug}" == "true" ]; then
+	echo ""
+	echo -e "\033[38;5;215mMutating ...\033[m"
+	echo "${sfd1}"
+	echo ""
+fi
+
+sfd1=$(echo "${sfd1}" | sed -e "\/dev\/${src_base}1/s/size=[^,]*/size= ${size1}/")
+
+if [ "${debug}" == "true" ]; then
+	echo ""
+	echo "${sfd1}"
+	echo ""
+fi
 
 p1_start=$(echo ${sfd0} | grep -Po "\/dev\/${src_base}1 : start= \K[^ ,]*")
-p2_start_orig=$(echo ${sfd1} | grep -Po "\/dev\/${src_base}2 : start= \K[^ ,]*")
-p2_start_new=$((p1_start + size1))
-sfd1=$(echo "${sfd1}" | sed -e "s/${p2_start_orig}/${p2_start_new}/")
+p2_start=$((p1_start + size1))
 
-# echo "${sfd1}"
+sfd1=$(echo "${sfd1}" | sed -e "s/P2_START/${p2_start}/")
 
-p2_size_orig=$(echo ${sfd0} | grep -Po "\/dev\/${src_base}2 : start= [0-9]*, size= \K[^ ,]*")
-p2_size_new=$((total_size - p2_start_new))
-sfd1=$(echo "${sfd1}" | sed -e "s/${p2_size_orig}/${p2_size_new}/")
+if [ "${debug}" == "true" ]; then
+	echo ""
+	echo "${sfd1}"
+	echo ""
+fi
 
-#sfd1=$(echo "${sfd1}" | sed -e "s/${src_dev}/${dst_dev}/g")
+p2_size=$((total_size - p2_start))
+sfd1=$(echo "${sfd1}" | sed -e "s/P2_SIZE/${p2_size}/")
+sfd1=$(echo "${sfd1}" | sed -e "s/${src_base}/${dst_base}/g")
+sfd1=$(echo "${sfd1}" | sed -e "s/${src_dev}/${dst_dev}/g")
 
 echo -e "\033[38;5;82m\nNew partition scheme\033[m"
 echo "${sfd1}"
 
-total=$((p2_start_new + p2_size_new))
+total=$((p2_start + p2_size))
 total_size=$((total * 512 / 1024 / 1024))
 
-echo "---"
+echo ""
 echo "Last block = $((total - 1))  Total size = ${total_size} MB"
+echo ""
 
 # ===
+
+exit
 
 function log() {
 	echo -e "\033[38;5;123m${@}\033[m"
 }
 
 echo
-log "Using dd the first 4 MB ..."
+log "Using dd for the first 4 MB ..."
 
 dd if=/dev/${src_dev} of=/dev/${dst_dev} bs=1M count=4
 
@@ -56,30 +79,30 @@ echo
 log "Applying new parition scheme ..."
 
 sfdisk --force /dev/${dst_base} > /dev/null <<< "${sfd1}"
-
 sync
 
-sleep 1
+#sleep 1
 
-partprobe /dev/${dst_dev}
+# partprobe /dev/${dst_dev}
 
-sleep 1
+# sleep 1
 
 yes | mkfs -t vfat -F 32 /dev/${dst_base}1
 yes | mkfs -t ext4 -L ${label} /dev/${dst_base}2
 
-echo
-log "Synchronizing file systems ..."
+sync
+
+log "Synchronizing file systems ... (be patient)"
 
 rsync_options="--force -rltWDEHXAgoptx"
 
 rm -rf /mnt/clone
-
 mkdir -p /mnt/clone
 
 mount /dev/${dst_base}2 /mnt/clone
-
+sync
 rsync ${rsync_options} --delete --exclude '.gvfs' --exclude '/dev/*' --exclude '/mnt/clone/*' --exclude '/proc/*' --exclude '/run/*' --exclude '/sys/*' --exclude '/tmp/*' // /mnt/clone
+sync
 
 if [ ! -d /mnt/clone/boot ]; then
 	log "Creating /mnt/clone/boot ... (usually not necessary)"
@@ -87,9 +110,8 @@ if [ ! -d /mnt/clone/boot ]; then
 fi
 
 mount /dev/${dst_base}1 /mnt/clone/boot
-
+sync
 rsync ${rsync_options} --delete --exclude '.gvfs' --exclude 'lost\+found/*' /boot/ /mnt/clone/boot
-
 sync
 
 df -h
